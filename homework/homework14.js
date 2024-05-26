96addf73f7c03c0e1650f2e2079c041877132056
MykolaSh96 <kolya20061@gmail.com>
2024-05-26 00:24:07 -0500

    Homework14

--- homework/homework14.js
+++ homework/homework14.js
@@ -15,6 +15,27 @@ repeatingX("xxxxx") 		-> true
 
  */
 
+// const repeatingX = (str) => {
+//   let str1 = str.toLowerCase()
+
+//   for(let i = 0; i < str1.length - 1; i++ ){
+//     if(str1[i] ==='x' && str1[i + 1] ==='x') return true;
+//   }
+//   return false;
+// }
+
+
+const repeatingX = (str) => str.toLowerCase().index('xx') !== -1;
+
+console.log(repeatingX("xTechxGlobalx") );  //-> false
+console.log(repeatingX("Hello Xx World"));  //	-> true
+console.log(repeatingX("x x") 		      );  // -> false
+
+console.log(repeatingX("") 	            );  //-> false
+console.log(repeatingX("xxxxx") 		    );  // -> true
+
+
+
 console.log('\n----------Task-02----------');
 
 /**
@@ -119,13 +140,18 @@ sumOfEvenNumbers( [ 10, 20, 30, 40, 50 ] ) 		-> 150
  */
 
 
-const sumOfEvenNumbers = (num) => {
-  let sum = 0;
-  for(numbers of num){
-    if(numbers % 2 === 0 )sum +=num;
-  }
-  return sum
-}
+// const sumOfEvenNumbers = (arr)=> {
+//  let sum = 0;
+
+//  for(const num of arr){
+//   if(num % 2 === 0){
+//     sum += num;
+//   }
+//  }
+// return sum;
+// }
+
+const sumOfEvenNumbers = (arr) => arr.reduce((sum, num) => num % 2 === 0 ? sum + num : sum, 0);
 
 
 console.log(sumOfEvenNumbers( [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] ) 	  ); //	-> 30
@@ -134,5 +160,45 @@ console.log(sumOfEvenNumbers( [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 ] ) ); //-> 0
 console.log(sumOfEvenNumbers( [ ] ) 			                            ); // -> 0
 console.log(sumOfEvenNumbers( [ 1, 2, 3, 4, 5 ] ) 			              ); // -> 6
 console.log(sumOfEvenNumbers( [ 10, 20, 30, 40, 50 ] ) 		            ); // -> 150
+
+
+
+console.log('/n----------task-5----------\n');
+
+
+/**
+ * Requirement:
+Write a function named capsOdds() which takes an array argument and returns the array 
+with all the odd index elements capitalized (converted to uppercase).
+
+Examples:
+capsOdds(["Hello", "World"]) 			-> ["Hello", "WORLD"]
+capsOdds(["Jan", "Feb", "Mar", "Apr"]) 		-> ["Jan", "FEB", "Mar", "APR"]
+capsOdds(["Apple", "Banana", "123", "456", "Peach", "Kiwi"]) 	-> ["Apple", "BANANA", "123", "456", "Peach", "KIWI"]
+capsOdds([ ]) 				-> [ ]
+capsOdds(["John !@#$%", "^&*() Doe"]) 		-> ["John !@#$%", "^&*() DOE"]
+
+ */
+
+// const capsOdds = (arr) => {
+//   let  newArr = [];  
+// for(let i = 0; i < arr.length; i++){
+//   if(i % 2 !== 0){
+//      newArr.push(arr[i].toUpperCase())
+//   }else {
+//     newArr.push(arr[i])
+//   }
+//   }
+//   return newArr;
+// } 
+const capsOdds = (arr) => arr.map((words, index) => index % 2 !==0 ? words.toUpperCase(): words);
+
+
+console.log(capsOdds(["Hello", "World"]) 			                          );  // -> ["Hello", "WORLD"]
+console.log(capsOdds(["Jan", "Feb", "Mar", "Apr"]) 	                    );  //-> ["Jan", "FEB", "Mar", "APR"]
+console.log(capsOdds(["Apple", "Banana", "123", "456", "Peach", "Kiwi"]));  //-> ["Apple", "BANANA", "123", "456", "Peach", "KIWI"]
+console.log(capsOdds([ ]) 			                                        );  //-> [ ]
+console.log(capsOdds(["John !@#$%", "^&*() Doe"]) 	                    );  //-> ["John !@#$%", "^&*() DOE"]
+
 
 
